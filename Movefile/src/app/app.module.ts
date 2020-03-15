e3ebed1e870cd3f31bdac05050773c3fdd5e2b47
qazqazzaq2 <savezts2@gmail.com>
2020-03-15 17:13:52 +0700

    safeis

---
+++ Movefile/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBF-G9AuFHQ3TNwg25Ft27F3Cknq9GQiqE',
+      authDomain: 'movefile-58783.firebaseapp.com',
+      storageBucket: 'movefile-58783.appspot.com',
+      projectId: 'movefile-58783',
+    }),
+    AngularFireStorageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
